46ca6c29fba1d74d0374691dac23908c6a809077
sidhant bhardwaj <142473258+sidhant0171@users.noreply.github.com>
2024-06-09 10:29:01 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost";
 $db_user = "root";
-$db_password = "0171";
-$db_name = "xportsoft";
+$db_password = "";
+$db_name = "";
 
 
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
